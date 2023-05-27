c138d6b6ae325c163ea0b09e5a8fac5ae7a7e10e
MSQureshi12156 <salmanqureshi5151@gmail.com>
2023-05-27 15:08:04 +0500

    Contact Us Page Changes

--- src/component/contact-Us.js
+++ src/component/contact-Us.js
@@ -10,7 +10,7 @@ const ContactUs = () => {
       <header>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container">
-            <a className="navbar-brand" href="#">My Website</a>
+            <a className="navbar-brand" href="#">LMS</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
               aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>

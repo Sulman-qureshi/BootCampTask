01b68e0013c55ce6b4ccff9be621a0fd229fae59
abdullahamir <abdullahamir706@gmail.com>
2023-05-30 22:27:30 +0500

    About Us Page Added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import AboutUs from './AboutUs';
 function App() {
   return (
     <div className="App">
-      {/* <AboutUs/> */}
+      <AboutUs/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
